c5b3effdd513200f9c7b7dfaeabcf93d3c7ebd70
Zeyad Eissa <dessouki98.ziad@gmail.com>
2025-03-04 12:19:03 +0100

    Update 24- B. Petya and Countryside.js

--- 24- B. Petya and Countryside.js
+++ 24- B. Petya and Countryside.js
@@ -1,1 +1,37 @@
-ewecs
+let input = "";
+process.stdin.on("data", (data) => {
+    if (data.toString().trim() === "end") {
+        process.stdin.emit("end")
+    }
+    input += data.toString()
+})
+
+process.stdin.on("end", () => {
+    let arr = input.split("\n")[1].split(" ").map(Number);
+    let max = 1;
+    for (let i = 0; i < arr.length; i++) {
+        max = Math.max(SlicingWindow(i, arr), max)
+    }
+    console.log(max);
+})
+const SlicingWindow = (index, arr) => {
+    if (index >= arr.length || index < 0) {
+        return 0;
+    }
+    let target = arr[index];
+    let right = 0;
+    for (let i = index + 1; i < arr.length; i++) {
+        if (arr[i] > arr[i - 1]) {
+            break;
+        }
+        right++;
+    }
+    let left = 0;
+    for (let i = index - 1; i > -1; i--) {
+        if (arr[i] > arr[i + 1]) {
+            break;
+        }
+        left++;
+    }
+    return left + right + 1;
+}
